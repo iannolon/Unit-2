6194234f78948e7cbe4b1056efa2ae40982ddd9c
iannolon <35769152+iannolon@users.noreply.github.com>
2018-01-29 08:31:39 -0500

    Create movie.py

---
+++ movie.py
@@ -0,0 +1,11 @@
+#SwollenNolon
+#1/29/18
+#movie.py
+
+age = int(input('What\'s your age?'))
+if (age >= 17):
+    print ('You can watch anything! Go wild.')
+elif (age >= 13):
+    print ('You can watch G, PG, and PG-13 movies by yourself, or R movies with an accompanying parent or guardian.')
+else:
+    print ('You can watch G and PG movies.')
