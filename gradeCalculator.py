a4efdc79146469b92c045c313bd7f740092f16b0
iannolon <35769152+iannolon@users.noreply.github.com>
2018-01-29 08:24:56 -0500

    Create gradeCalculator.py

---
+++ gradeCalculator.py
@@ -0,0 +1,15 @@
+#SwollenNolon
+#1/29/18
+#gradeCalculator.py
+
+grade = int(input('What\'s your grade?'))
+if (grade >= 90):
+    print ('You earned an A!')
+elif (grade >= 80):
+    print ('You earned a B')
+elif (grade >= 70):
+    print ('You earned a C')
+elif (grade >= 60):
+    print ('You earned a D')
+else:
+    print ('You falied :(')
