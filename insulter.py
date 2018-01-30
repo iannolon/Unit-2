d55845b93737cb249f223592fb6d651fce58a6eb
iannolon <35769152+iannolon@users.noreply.github.com>
2018-01-30 08:28:17 -0500

    Create insulter.py

---
+++ insulter.py
@@ -0,0 +1,19 @@
+#SwollenNolon
+#1/29/18
+#insulter.py
+
+#If I had a death wish, I'd climb to your ego and jump to your IQ.
+
+from random import randint
+name = input('Enter your name: ')
+num = randint(1,5)
+if num == 1:
+    print('If I had a death wish, I\'d climb to your ego and jump to your IQ.')
+elif num == 2:
+    print('Your mama so old, she sat next to Jesus in kindergarten!')
+elif num == 3:
+    print('It\'s not easy being green. It\'s also not easy coming up with these insults. Also, you should wear more deodorant.')
+elif num == 4:
+    print('You look like somebody who just hit puberty. And puberty hit back.')
+elif num == 5:
+    print('You look like the kind of person who failed PE. And it wasn\'t because you talked too much.')
