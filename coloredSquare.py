115bd1d65482f173e3988949d198e6405fdb5150
iannolon <35769152+iannolon@users.noreply.github.com>
2018-01-31 08:36:29 -0500

    Update coloredSquare.py

--- coloredSquare.py
+++ coloredSquare.py
@@ -1,1 +1,27 @@
+#IanNolon
+#1/31/18
+#coloredSquare.py
 
+from ggame import *
+from random import randint
+red = Color(0xff0000,1)
+redline = LineStyle(3,red)
+blue = Color(0x0000ff,1)
+blueline = LineStyle(3,blue)
+green = Color(0x00cc00,1)
+greenline = LineStyle(3,green)
+yellow = Color(0xffff00,1)
+yellowline = LineStyle(3,yellow)
+rancol = randint(1,4)
+if rancol == 1:
+    rectangle = RectangleAsset(100,100,redline,red)
+if rancol == 2:
+    rectangle = RectangleAsset(100,100,blueline,blue)
+if rancol == 3:
+    rectangle = RectangleAsset(100,100,greenline,green)
+if rancol == 4:
+    rectangle = RectangleAsset(100,100,yellowline,yellow)
+
+Sprite (rectangle)
+myApp = App()
+myApp.run()
