41537ec92acf6b6c9082d0d3537e93cf06a69707
iannolon <35769152+iannolon@users.noreply.github.com>
2018-02-02 08:37:05 -0500

    Create lastName.py

---
+++ lastName.py
@@ -0,0 +1,9 @@
+#IanNolon
+#2/2/18
+#lastname.py
+
+apellido = str(input('What\'s your last name?'))
+if apellido < 'N':
+    print(apellido,'is in the first half of the alphabet')
+if apellido > 'M':
+    print(apellido,'is in the second half of the alphabet')
