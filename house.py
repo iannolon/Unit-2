6edaf1fb57e7416d2a01cef95dfdbf09727dbbc2
iannolon <35769152+iannolon@users.noreply.github.com>
2018-02-09 08:16:08 -0500

    Update house.py

--- house.py
+++ house.py
@@ -6,23 +6,20 @@ from ggame import *
 
 red = Color(0xFF0000,1) #this is color red
 green = Color(0x00FF00,1)
-blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
+white = Color(0xFFFFFF,1)
 
 blackOutline = LineStyle(1,black)
 
-houseBody = RectangleAsset(300,360,blackOutline,green) #width, height, outline, fill
-blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, fill
-greenEllipse = EllipseAsset(100,50,blackOutline,green) #width, height, outline, fill
-blackLine = LineAsset(50,160,blackOutline) #x_endpoint, y_endpoint, lineStyle
-redTriangle = PolygonAsset([(0,0),(120,180),(60,300)], blackOutline, red) #endpoints, outline, fill
-text = TextAsset('Swollen Nolon', fill = black, style = 'bold 40pt Times') #text
+houseBody = RectangleAsset(300,260,blackOutline,green) #width, height, outline, fill
+roof = PolygonAsset([(400,400),(700,400),(550,250)], blackOutline, red) #endpoints, outline, fill
+door = RectangleAsset(70,130,blackOutline,black) #width, height, outline, fill
+window1 = RectangleAsset(60,60,blackOutline,white) #width, height, outline, fill
+window2 = RectangleAsset(60,60,blackOutline,white) #width, height, outline, fill
 
-
-Sprite(houseBody,(350,150))
-#Sprite(redTriangle)
-#Sprite(blueCircle,(50,50))
-#Sprite(greenEllipse,(200,200))
-#Sprite(blackLine)
-#Sprite(text,(300,200))
+Sprite(houseBody,(350,220))
+Sprite(roof,(350,70))
+Sprite(door,(450,350))
+Sprite(window1,(380,260))
+Sprite(window2,(530,260))
 App().run()
